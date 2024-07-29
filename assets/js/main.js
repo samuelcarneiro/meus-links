8643e2398aa1819c464934cfe855dc735de6a851
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-07-29 11:42:15 -0300

    Alteração da função toggleMode e salvo a preferencia do thema escolhido usando o LocalStorage do navegador para 'replicar' a escolha em todas as páginas do site

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,31 @@ function toggleMode() {
     // Pegar o document html
     const html = document.documentElement
 
-    // Se no document html contém a classe light
-    // if(html.classList.contains('light')) {
-    //     Pegar o html (variável a cima) e remover a classe light do document
-    //    html.classList.remove('light')
-    // } else {
-    //     Se não conter a a classe light, adiciona-a no document
-    //    html.classList.add('light')
-    // }
+    //Se no document html contém a classe light
+    if(html.classList.contains('dark')) {
+        html.classList.replace('dark', 'light')
+        localStorage.setItem('theme', 'light')
+    } else {
+        html.classList.replace('light', 'dark')
+        localStorage.setItem('theme', 'dark')
+    }
 
     // A função toggle já faz o que foi declarado na função a cima...
-    html.classList.toggle('light')
+    //html.classList.toggle('light')
     // Se tiver a classe light ele tira. Se não tiver a classe light, ele coloca
 }
+
+// Ao carregar a página
+const savedTheme = localStorage.getItem('theme');
+const html = document.documentElement;
+
+if (savedTheme) {
+    html.classList.add(savedTheme);
+} else {
+    // Se não houver preferência armazenada, aplicar um tema padrão
+    html.classList.add('light');
+}
+
 
 /* Mostrar ano atual no footer */
 const ano = document.getElementById("mostrarAnoAtual");
